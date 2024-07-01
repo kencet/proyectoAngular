f7a94240c31cc7f22934864b9804e496ee87332f
peter <peterjcarvajalp@gmail.com>
2024-07-01 15:28:40 -0400

    Tercer commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {Task} from "./../../models/task.model";
 
 @Component({
   selector: 'app-home',
@@ -10,21 +11,36 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks =  signal([
+  tasks = signal<Task[]>([
     {
     id: Date.now(),
     tittle: "Crear proyecto",
     completed: false
     },
-    "pawa"
+    {
+      id: Date.now(),
+      tittle: "Crear proyecto2",
+      completed: false
+      },
   ]);
 
   changeHandler(event : Event) {
     const input = event.target as HTMLInputElement
     const newTask = input.value
-    this.tasks.update((tasks) => [...tasks, newTask]);
+
     input.value="";
   }
+    
+  addTasks (tittle: string) {
+  const newTask = {
+    id: Date.now(),
+    tittle, 
+    completed: false
+  }
+  this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+
   deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((tasks, position) => position !== index));
   }
